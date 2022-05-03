f88679e4c118d0231bcef9e503e9da0b9aba728c
SeungHyun Kim <k10130219@gmail.com>
2022-05-03 15:33:26 +0900

    Fix Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    jsxFactory: 'jsx',
-    jsxInject: `import { jsx } from '@emotion/react'`,
+    jsxFactory: "jsx",
+    jsxInject: `import { jsx } from '@emotion/react'`
   },
-  plugins:  [
+  plugins: [
     react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
+      jsxImportSource: "@emotion/react"
+      // babel: {
+      //   plugins: ["@emotion/babel-plugin"],
+      // },
+    })
   ]
-})
+});
